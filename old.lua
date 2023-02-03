47b8e9d36287c290fdc115dba280e364a14b15f6
szze <106690765+CF-Trail@users.noreply.github.com>
2023-02-04 01:58:42 +0300

    Update old.lua

--- old.lua
+++ old.lua
@@ -378,7 +378,12 @@ local sNames = {
 	"fontSize",
 	'noFont',
 	'taggedBoothHop',
-	'rainbowText'
+	'rainbowText',
+	'helloResponce',
+	'botResponce',
+	"donateResponce",
+	"otherResponce",
+	"scamResponce"
 }
 
 local positionX = workspace:WaitForChild('Boomboxes'):WaitForChild('Spawn')
@@ -436,7 +441,31 @@ local sValues = {
 	5,
 	false,
 	false,
-	false
+	false,
+	{
+		'Hi',
+		'Sup',
+		'Hello'
+	},
+	{
+		'no im not',
+		'im not a bot'
+	},
+	{
+		"sorry im saving",
+		"i am saving for my dream item",
+		"sorry my robux is pending"
+	},
+	{
+		"..?",
+		"what",
+		"?"
+	},
+	{
+		"im no scam",
+		"im not a scammer",
+		"this is not a scam"
+	}
 }
 
 if #getgenv().settings ~= sNames then
@@ -1525,7 +1554,7 @@ msgdone.OnClientEvent:Connect(function(msgdata)
 	local message = string.lower(msgdata.Message)
 	task.wait(2.1 + math.random(0.4, 1))
 	local plrChatted = game:GetService('Players')[speaker] or nil
-	if (plrChatted and plrChatted == game:GetService('Players').LocalPlayer) or getgenv().settings.autoNearReply == false or not plrChatted then
+	if (plrChatted and plrChatted == game:GetService('Players').LocalPlayer) or getgenv().settings.autoNearReply == false or not plrChatted  or string.find(message, 'donates') or string.find(message, "spamming") then
 		return
 	end
 	pcall(function()
@@ -1534,19 +1563,15 @@ msgdone.OnClientEvent:Connect(function(msgdata)
 			local root = chatChar.Humanoid.RootPart
 			if (root.Position - game:GetService('Players').LocalPlayer.Character.Humanoid.RootPart.Position).Magnitude < 11 then
 				if message == 'hello' or message == 'hi' or message == 'sup' or message == 'hey' then
-					messageRequest:FireServer("hello", 'All')
-				elseif string.find(message, 'jump') then
-					messageRequest:FireServer('ok', 'All')
-				elseif string.find(message, '?') and not string.find(message, 'bot') then
-					messageRequest:FireServer('yes', 'All')
+					messageRequest:FireServer(getgenv().settings.helloResponce[math.random(1, #getgenv().settings.helloResponce)], 'All')
 				elseif string.find(message, 'bot') then
-					messageRequest:FireServer(randombotmsgs[math.random(1, #randombotmsgs)], 'All')
+					messageRequest:FireServer(getgenv().settings.botResponce[math.random(1, #getgenv().settings.botResponce)], 'All')
 				elseif string.find(message, 'donate') then
-					messageRequest:FireServer('no', 'All')
+					messageRequest:FireServer(getgenv().settings.donateResponce[math.random(1, #getgenv().settings.donateResponce)], 'All')
 				elseif string.find(message, 'scam') then
-					messageRequest:FireServer('no i dont scam', 'All')
+					messageRequest:FireServer(getgenv().settings.scamResponce[math.random(1, #getgenv().settings.scamResponce)], 'All')
 				else
-					messageRequest:FireServer(randommsgs[math.random(1, #randommsgs)], 'All')
+					messageRequest:FireServer(getgenv().settings.otherResponce[math.random(1, #getgenv().settings.otherResponce)], 'All')
 				end
 			end
 		end
