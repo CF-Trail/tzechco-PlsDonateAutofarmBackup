40263cfce5dacf3e794c2c69265bc941d414fd06
szze <106690765+CF-Trail@users.noreply.github.com>
2023-02-13 23:33:23 +0300

    Update old.lua

--- old.lua
+++ old.lua
@@ -33,7 +33,7 @@ if isfile and writefile and typeof(isfile) == 'function' and typeof(writefile) =
 		writefile('PromptedDiscordCFCommunityPDDD.txt', game:GetService('HttpService'):JSONEncode('hi'))
 		local Module = loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/random/main/DiscordModule"))()
 		Module.Prompt({
-			invite = "https://discord.gg/kZZHDSVPvy",
+			invite = "https://discord.gg/bthg",
 			name = "bug reports & suggestions", -- update
 		})
 	end
