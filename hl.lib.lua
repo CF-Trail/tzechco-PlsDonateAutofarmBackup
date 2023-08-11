bcdab8bbddd6b1438f7cd2c184db9c4581d6a40d
szze <106690765+CF-Trail@users.noreply.github.com>
2023-08-11 18:05:08 +0300

    Update hl.lib.lua

--- hl.lib.lua
+++ hl.lib.lua
@@ -15,44 +15,74 @@ local function chat(C_1:string)
 	end
 end
 
+local function breakVelocity(char)
+    local isVeloBroken = false
+    task.delay(1,function()
+        isVeloBroken = true
+    end)
+    while not isVeloBroken and task.wait() do
+        for i,v in next, char:GetChildren() do
+            if v:IsA('BasePart') then
+                v.Velocity = v.Velocity * Vector3.new(0,1,0)
+            end
+        end
+    end
+end
+
 local _CFRAMETABLE = {{-30.9582253, 1.36195695, 100.472855},{13.1596403, 0.731242418, 152.176956},{69.3110199, 0.73332727, 150.993561},{92.6021729, 0.733850241, 130.599258},{96.9429169, 0.738471866, 75},{91.9788284, 0.762879133, 72.3707657},{70.7379608, 0.737098873, 50.9553299},{15.6208801, 0.751551986, 50.5339737},{{-30.9582253, 1.36195695, 100.472855}}}
 
 function lib.HLSetup(char)
-    --[[for i,v in next, _CFRAMETABLE do
-        local _nPartC = Instance.new('Part',workspace)
-        _nPartC.Name = '_[lap//]'
-        _nPartC.Anchored = true
-        _nPartC.Transparency = 1
-        _nPartC.CanCollide = false
-        _nPartC.CFrame = CFrame.new(table.unpack(i))
-    end]]
+
 end
 
 
 function lib.HLUnload(char)
-    --while workspace:FindFirstChild('_[lap//]') and task.wait() do
-   --     workspace:FindFirstChild('_[lap//]'):Destroy()
- --   end
+
 end
 
 function lib.HLStart(char,raised)
-    twn(char.Humanoid.RootPart,TweenInfo.new(5,Enum.EasingStyle.Linear,Enum.EasingDirection.In),{CFrame = CFrame.new(-30.9582253, 1.36195695, 100.472855)}):Play()
-    task.wait(6)
-    for _i = 1, raised do
-        for i,v in next, _CFRAMETABLE do
-            local con
-            local _mtfinish
-            char.Humanoid:MoveTo(Vector3.new(unpack(v)))
-            con = char.Humanoid.MoveToFinished:Connect(function()
-                _mtfinish = true
-                con:Disconnect()
+    task.wait(1)
+    local oldPos = char.Humanoid.RootPart.CFrame
+    local minMagni = 9999999999999
+    local hitChosen = nil
+    for i,v in next, workspace.Map.Launchpads:GetChildren() do
+        local hit = v:FindFirstChild('Trampoline_Hitbox')
+        if hit then
+            local mag = (char.Humanoid.RootPart.Position - hit.Position).Magnitude
+            if mag < minMagni then
+                hitChosen = hit
+                minMagni = mag
+            end
+        end
+    end
+    hitChosen.LaunchPower.Value = 90
+    twn(char.Humanoid.RootPart,TweenInfo.new(5,Enum.EasingStyle.Linear,Enum.EasingDirection.In),{CFrame = CFrame.new(hitChosen.Position + Vector3.new(0,5,0))}):Play()
+    local n_con
+    local touches = 0
+    local debounce = false
+    local blockedStates = {Enum.HumanoidStateType.Ragdoll,Enum.HumanoidStateType.FallingDown}
+    for i,v in next, blockedStates do
+        char.Humanoid:SetStateEnabled(v,false)
+    end
+    n_con = hitChosen.Touched:Connect(function(v)
+        if v:IsDescendantOf(char) and not debounce then
+            touches += 1
+            debounce = true
+            task.delay(0.3,function()
+                debounce = false
             end)
-            repeat task.wait() until _mtfinish == true
-        end 
+        end
+        if touches >= raised then
+            warn('[CF] Disconnecting Trampoline Touch Connection')
+            n_con:Disconnect()
+            n_con = nil
+        end
+    end)
+    while n_con do
+        breakVelocity(char)
+        task.wait()
     end
-    if walkToBooth then
-        walkToBooth()
-    end
+    twn(char.Humanoid.RootPart,TweenInfo.new(5,Enum.EasingStyle.Linear,Enum.EasingDirection.In),{CFrame = oldPos}):Play()
 end
 
 return lib
