f52444739d0ec3832d4c24d0595645257e2ff4fd
szze <106690765+CF-Trail@users.noreply.github.com>
2023-12-06 01:48:11 +0300

    Update old.lua

--- old.lua
+++ old.lua
@@ -847,7 +847,7 @@ if game:GetService('CoreGui'):FindFirstChild('RobloxPromptGui') then
 	end)
 end
 
-local Window = library:AddWindow("@szze | Happy Thanksgiving!",
+local Window = library:AddWindow("@szze | tinyurl.com/szzes",
   {
 	main_color = Color3.fromRGB(80, 80, 80),
 	min_size = Vector2.new(560, 563),
