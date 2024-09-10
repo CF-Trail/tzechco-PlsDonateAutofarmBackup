b0bcd8f28467d34276e63907d5cd2d2e44cb8c10
szze <106690765+CF-Trail@users.noreply.github.com>
2024-09-10 22:23:32 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -864,7 +864,7 @@ if game:GetService('CoreGui'):FindFirstChild('RobloxPromptGui') then
 	end)
 end
 
-local Window = library:AddWindow("reaching out for immigration support. please contact in 'supportion' game.",
+local Window = library:AddWindow("@szze | живу в черно-белом миру",
   {
 	main_color = Color3.fromRGB(80, 80, 80),
 	min_size = Vector2.new(560, 563),
