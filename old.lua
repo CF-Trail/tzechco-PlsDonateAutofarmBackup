fa7863a70e166013d5ba51ef33cc8ea39225b5d0
szze <106690765+CF-Trail@users.noreply.github.com>
2024-09-29 21:43:17 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -13,6 +13,7 @@ if game.PlaceId ~= 8737602449 and game.PlaceId ~= 8943844393 then
 	return
 end
 
+local identifyexecutor = identifyexecutor or function() return 'Unknown' end
 local cloneref = (identifyexecutor() ~= "Synapse Z" and cloneref) or function(o) return o end -- infinite yield
 local CoreGui = cloneref(game:GetService("CoreGui"))
 local Players = cloneref(game:GetService("Players"))
