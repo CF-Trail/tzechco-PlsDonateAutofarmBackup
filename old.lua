a38eb8359176bb026604fead3fe4747442b20137
szze <106690765+CF-Trail@users.noreply.github.com>
2024-10-07 08:55:14 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -939,7 +939,7 @@ local function checkForBots()
 	end
 end
 
-local Window = library:AddWindow("Discord: @szze | fucking kill me",
+local Window = library:AddWindow("@szze, i suppose.",
   {
 	main_color = Color3.fromRGB(80, 80, 80),
 	min_size = Vector2.new(560, 563),
