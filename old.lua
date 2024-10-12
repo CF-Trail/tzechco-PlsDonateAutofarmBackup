25fc3427e86c68dabb90538f33af6fed61a7397e
szze <106690765+CF-Trail@users.noreply.github.com>
2024-10-13 04:33:30 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -939,7 +939,7 @@ local function checkForBots()
 	end
 end
 
-local Window = library:AddWindow("@szze | Goodnight, night farmers 💤",
+local Window = library:AddWindow("emptiness",
   {
 	main_color = Color3.fromRGB(80, 80, 80),
 	min_size = Vector2.new(560, 563),
