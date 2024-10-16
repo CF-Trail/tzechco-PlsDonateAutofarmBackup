68606db3e05e379750c45220d8d20a1b968c7c0d
szze <106690765+CF-Trail@users.noreply.github.com>
2024-10-17 07:51:52 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -1667,7 +1667,7 @@ end
 local function boothclaim()
 	require(ReplicatedStorage.Remotes).Event("ClaimBooth"):InvokeServer(unclaimed[2])
 	if not string.find(_boothlocation.BoothUI:FindFirstChild(tostring("BoothUI" .. unclaimed[2])).Details.Owner.Text, Players.LocalPlayer.DisplayName) then
-		task.wait(4)
+		task.wait(1)
 		if not string.find(_boothlocation.BoothUI:FindFirstChild(tostring("BoothUI" .. unclaimed[2])).Details.Owner.Text, Players.LocalPlayer.DisplayName) then
 			error()
 		end
