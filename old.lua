9666871417c36b44bd74f290a616b64a6f4b864d
szze <106690765+CF-Trail@users.noreply.github.com>
2024-10-25 04:54:07 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -947,9 +947,9 @@ local function checkForBots()
 	end
 end
 
-local Window = library:AddWindow("@szze | тераблейд )) на моих рогах сидят 2две суки ) )))))",
+local Window = library:AddWindow("@szze | 🎃",
   {
-	main_color = Color3.fromRGB(80, 80, 80),
+	main_color = Color3.fromRGB(255, 149, 80),
 	min_size = Vector2.new(560, 563),
 	toggle_key = Enum.KeyCode.RightShift,
 	can_resize = true,
