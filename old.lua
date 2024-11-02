fa9afac1b35fbc9305ba400c30d25a108c75bd20
szze <106690765+CF-Trail@users.noreply.github.com>
2024-11-03 06:37:21 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -947,7 +947,7 @@ local function checkForBots()
 	end
 end
 
-local Window = library:AddWindow("@szze",
+local Window = library:AddWindow("@szze | tiktok.com/@axikoqqe/live",
   {
 	main_color = Color3.fromRGB(80, 80, 80),
 	min_size = Vector2.new(560, 563),
