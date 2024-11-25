91dc1a62004b5656e311c5f3fffa95a65adcf33d
szze <106690765+CF-Trail@users.noreply.github.com>
2024-11-25 19:23:31 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -966,7 +966,7 @@ local function checkForBots()
 	end
 end
 
-local Window = library:AddWindow("@szze | 🇺🇸",
+local Window = library:AddWindow("@szze | lf geoguessr team, dm me",
   {
 	main_color = Color3.fromRGB(80, 80, 80),
 	min_size = Vector2.new(560, 563),
