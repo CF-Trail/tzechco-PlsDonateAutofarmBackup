3f08d23fa43225e9366f31883b89e558a8b900c4
szze <106690765+CF-Trail@users.noreply.github.com>
2024-12-07 20:22:33 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -14,12 +14,15 @@ if game.PlaceId ~= 8737602449 and game.PlaceId ~= 8943844393 then
 end
 
 local identifyexecutor = identifyexecutor or function() return 'Unknown' end
-local cloneref = (identifyexecutor() ~= "Synapse Z" and cloneref) or function(o) return o end -- infinite yield
+local cloneref = (identifyexecutor() ~= "Synapse Z" and not identifyexecutor():find("Codex") and cloneref) or function(o) return o end -- infinite yield
 local CoreGui = cloneref(game:GetService("CoreGui"))
 local Players = cloneref(game:GetService("Players"))
 local HttpService = cloneref(game:GetService("HttpService"))
 local TPService = cloneref(game:GetService("TeleportService"))
-local ReplicatedStorage = cloneref(game:GetService("ReplicatedStorage"))
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+if not workspace then
+	workspace = game:GetService('Workspace')
+end
 
 --skidded!!! ty tvk1308
 for k, v in pairs(getgc(true)) do
