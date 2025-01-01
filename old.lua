e85298c26d6865b283bb7dce3b0c2608837544ee
szze <106690765+CF-Trail@users.noreply.github.com>
2025-01-01 09:08:04 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -886,7 +886,7 @@ local function checkForBots()
 	end
 end
 
-local Window = library:AddWindow("@szze .gg/e5Tg9SFnrq | Merry christmas! 🎄 Spend time with your family!",
+local Window = library:AddWindow("@szze .gg/e5Tg9SFnrq | Happy New Year! 🎄",
   {
 	main_color = Color3.fromRGB(80, 80, 80),
 	min_size = Vector2.new(560, 563),
