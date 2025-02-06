56066a43dc8a621113c0ecb48215c1d88da8e186
szze <106690765+CF-Trail@users.noreply.github.com>
2025-02-07 02:28:56 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -886,7 +886,7 @@ local function checkForBots()
 	end
 end
 
-local Window = library:AddWindow("@szze // legit (!) rivals players pls dm",
+local Window = library:AddWindow("@szze .gg/yrQbdfhuqd",
   {
 	main_color = Color3.fromRGB(80, 80, 80),
 	min_size = Vector2.new(560, 563),
