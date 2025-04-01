760d21c7332637a61f02f51aaa23c7908826d602
szze <106690765+CF-Trail@users.noreply.github.com>
2025-04-01 21:58:21 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -886,7 +886,7 @@ local function checkForBots()
 	end
 end
 
-local Window = library:AddWindow("@szze .gg/yrQbdfhuqd | hi hi!!!",
+local Window = library:AddWindow("@szze .gg/yrQbdfhuqd | ROBLOX Rivals Script V2.6",
   {
 	main_color = Color3.fromRGB(80, 80, 80),
 	min_size = Vector2.new(560, 563),
