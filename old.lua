3efcea75ef8ccc826befae0aad1d691bc0916612
szze <106690765+CF-Trail@users.noreply.github.com>
2025-08-10 12:09:52 +0900

    Update old.lua

--- old.lua
+++ old.lua
@@ -753,7 +753,7 @@ local easterlol = {
 
 local easterclr = easterlol[math.random(1,#easterlol)]
 
-local Window = library:AddWindow("@szze (gay gay homosexual gay) .gg/yrQbdfhuqd",
+local Window = library:AddWindow("if your mental state is bad, don't make it untreated.",
   {
 	main_color = easterclr,
 	min_size = Vector2.new(560, 563),
